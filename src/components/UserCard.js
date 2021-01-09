2d3b79c972c2cf5946233d1d2dbe09104a6dbed2
Hazel <hazelwence_13@hotmail.com>
2021-01-09 01:11:56 -0600

    fetching data from mock api

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,91 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const UserCard = () => {
+export const UserCard = (props) => {
+
+  const user = props.user;
+  
   return (
-    <div className="card-container">
-      <div className="user-card">      
-        <div className="info">
-          <h3><strong>Name: </strong>Lupita Ramirez Guanajuates</h3>
-          <h3><strong>UserName: </strong>@rayoasdasdasda</h3>
-          <h3><strong>Posts: </strong>20</h3>
-        </div>
-        <div className="post-titles"> 
-          <label>
-            <h3><strong>Post titles:</strong></h3>
-            <select name="select">
-              <option value="value1">Value 1</option>
-              <option value="value2" selected>Value 2</option>
-              <option value="value3">Value 3</option>
-            </select>
-          </label>         
-        </div>
+    
+    <div className="user-card">      
+      <div className="info">
+        <h3><strong>Name: </strong>{user.name}</h3>
+        <h3><strong>UserName: </strong>@{user.username}</h3>
+        <h3><strong>Posts: </strong>{user.posts}</h3>
       </div>
-
-      <div className="user-card">      
-        <div className="info">
-          <h3><strong>Name: </strong>Lupita Ramirez Guanajuates</h3>
-          <h3><strong>UserName: </strong>@rayoasdasdasda</h3>
-          <h3><strong>Posts: </strong>20</h3>
-        </div>
-        <div className="post-titles"> 
-          <label>
-            <h3><strong>Post titles:</strong></h3>
-            <select name="select">
-              <option value="value1">Value 1</option>
-              <option value="value2" selected>Value 2</option>
-              <option value="value3">Value 3</option>
-            </select>
-          </label>         
-        </div>
-      </div><div className="user-card">      
-        <div className="info">
-          <h3><strong>Name: </strong>Lupita Ramirez Guanajuates</h3>
-          <h3><strong>UserName: </strong>@rayoasdasdasda</h3>
-          <h3><strong>Posts: </strong>20</h3>
-        </div>
-        <div className="post-titles"> 
-          <label>
-            <h3><strong>Post titles:</strong></h3>
-            <select name="select">
-              <option value="value1">Value 1</option>
-              <option value="value2" selected>Value 2</option>
-              <option value="value3">Value 3</option>
-            </select>
-          </label>         
-        </div>
-      </div><div className="user-card">      
-        <div className="info">
-          <h3><strong>Name: </strong>Lupita Ramirez Guanajuates</h3>
-          <h3><strong>UserName: </strong>@rayoasdasdasda</h3>
-          <h3><strong>Posts: </strong>20</h3>
-        </div>
-        <div className="post-titles"> 
-          <label>
-            <h3><strong>Post titles:</strong></h3>
-            <select name="select">
-              <option value="value1">Value 1</option>
-              <option value="value2" selected>Value 2</option>
-              <option value="value3">Value 3</option>
-            </select>
-          </label>         
-        </div>
-      </div><div className="user-card">      
-        <div className="info">
-          <h3><strong>Name: </strong>Lupita Ramirez Guanajuates</h3>
-          <h3><strong>UserName: </strong>@rayoasdasdasda</h3>
-          <h3><strong>Posts: </strong>20</h3>
-        </div>
-        <div className="post-titles"> 
-          <label>
-            <h3><strong>Post titles:</strong></h3>
-            <select name="select">
-              <option value="value1">Value 1</option>
-              <option value="value2" selected>Value 2</option>
-              <option value="value3">Value 3</option>
-            </select>
-          </label>         
-        </div>
+      <div className="post-titles"> 
+        <label>
+          <h3><strong>Post titles:</strong></h3>
+          <select name="select">
+            <option value="value1">Value 1</option>
+            <option value="value2">Value 2</option>
+            <option value="value3">Value 3</option>
+          </select>
+        </label>         
       </div>
-    </div>
-  )
+    </div> 
+    
+  )  
 }
--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,14 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchBar } from './SearchBar';
 import { UserCard } from './UserCard';
 
 export const UsersTable = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [completeUsers, setcompleteUsers] = useState([]);
+
+  useEffect(async() => {    
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        console.log(response);
+      }
+    })
+    .then(json => {
+      var postTitlesByUser = json.map(post => {
+        return {
+          userId: post.userId,
+          title: post.title
+        };
+      });     
+
+      var postsByUser = postTitlesByUser.reduce((acc, curr) => {
+
+        if (acc[curr.userId] == null) {
+          acc[curr.userId] = { userId:curr.userId, posts: 1, titles: [curr.title] };
+        } else {
+          acc[curr.userId].posts++;
+          acc[curr.userId].titles.push(curr.title);
+        }
+
+        return acc;
+      }, {}); 
+
+      setPosts(postsByUser); 
+
+    });
+  });    
+
+  useEffect(async() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        console.log(response);
+      }
+    })
+    .then(response => {
+      let usersData = response.map( obj => {
+        return {
+          userId: obj.id, 
+          name: obj.name, 
+          username: obj.username
+        }
+      });
+
+      setUsers(usersData); 
+      
+      if(users.length > 0 ){
+    
+        let completeUsersArr = users.map( user => {           
+          if( user.userId == posts[user.userId].userId ){
+            return {
+              userId: user.userId,
+              name: user.name,
+              username: user.username,
+              posts: posts[user.userId].posts,
+              titles: posts[user.userId].titles
+            }
+          }
+        });   
+        
+        setcompleteUsers(completeUsersArr);
+              
+      }
+    });
+
+  }); 
+
   return (
     <div className="principal-container">
       <h1>Search for Users</h1>
       <div className="users-table">
         <SearchBar />
-        <UserCard />
+        <div className="card-container">
+        {
+          completeUsers.map(user => {            
+          return <UserCard key={user.userId} user={user} />
+          })
+        }
+        </div>
       </div>
     </div>
   )
