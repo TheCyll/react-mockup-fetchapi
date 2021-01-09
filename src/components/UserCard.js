ce86dce2260fa079d4537046c80aef7835ddca5d
Hazel <hazelwence_13@hotmail.com>
2021-01-09 12:34:57 -0600

    Search for cards and change of async function in useEffect

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,23 +3,25 @@ import React, { useEffect, useState } from 'react';
 export const UserCard = (props) => {
 
   const user = props.user;
-  
+
   return (
     
     <div className="user-card">      
       <div className="info">
         <h3><strong>Name: </strong>{user.name}</h3>
-        <h3><strong>UserName: </strong>@{user.username}</h3>
+        <h3><strong>UserName: </strong>{user.username}</h3>
         <h3><strong>Posts: </strong>{user.posts}</h3>
       </div>
       <div className="post-titles"> 
         <label>
-          <h3><strong>Post titles:</strong></h3>
+          <h3><strong>Post titles:</strong></h3>          
           <select name="select">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
+            {
+              user.titles.map( title => {                
+                return <option value={'title'}>{title}</option>
+              })
+            }  
+          </select>        
         </label>         
       </div>
     </div> 
--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -7,65 +7,73 @@ export const UsersTable = () => {
   const [posts, setPosts] = useState([]);
   const [users, setUsers] = useState([]);
   const [completeUsers, setcompleteUsers] = useState([]);
+  const [filteredtext, setfilteredText] = useState('');
 
-  useEffect(async() => {    
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        console.log(response);
-      }
-    })
-    .then(json => {
-      var postTitlesByUser = json.map(post => {
-        return {
-          userId: post.userId,
-          title: post.title
-        };
-      });     
-
-      var postsByUser = postTitlesByUser.reduce((acc, curr) => {
-
-        if (acc[curr.userId] == null) {
-          acc[curr.userId] = { userId:curr.userId, posts: 1, titles: [curr.title] };
+  useEffect(() => {  
+    async function  getData() {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
         } else {
-          acc[curr.userId].posts++;
-          acc[curr.userId].titles.push(curr.title);
+          console.log(response);
         }
+      })
+      .then(json => {
+        var postTitlesByUser = json.map(post => {
+          return {
+            userId: post.userId,
+            title: post.title
+          };
+        });     
 
-        return acc;
-      }, {}); 
+        var postsByUser = postTitlesByUser.reduce((acc, curr) => {
 
-      setPosts(postsByUser); 
+          if (acc[curr.userId] == null) {
+            acc[curr.userId] = { userId:curr.userId, posts: 1, titles: [curr.title] };
+          } else {
+            acc[curr.userId].posts++;
+            acc[curr.userId].titles.push(curr.title);
+          }
 
-    });
-  });    
+          return acc;
+        }, {}); 
 
-  useEffect(async() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        console.log(response);
-      }
-    })
-    .then(response => {
-      let usersData = response.map( obj => {
-        return {
-          userId: obj.id, 
-          name: obj.name, 
-          username: obj.username
-        }
+        setPosts(postsByUser); 
+
       });
-
-      setUsers(usersData); 
-      
-      if(users.length > 0 ){
+    }
     
-        let completeUsersArr = users.map( user => {           
-          if( user.userId == posts[user.userId].userId ){
+    getData();
+    
+  }, []);    
+
+  useEffect(() => {
+
+    async function getData(){
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          console.log(response);
+        }
+      })
+      .then(response => {
+        let usersData = response.map( obj => {
+          return {
+            userId: obj.id, 
+            name: obj.name, 
+            username: obj.username
+          }
+        });
+
+        setUsers(usersData); 
+        
+        if(users.length > 0 ){
+      
+          let completeUsersArr = users.map( user => { 
+            
             return {
               userId: user.userId,
               name: user.name,
@@ -73,24 +81,42 @@ export const UsersTable = () => {
               posts: posts[user.userId].posts,
               titles: posts[user.userId].titles
             }
-          }
-        });   
-        
-        setcompleteUsers(completeUsersArr);
-              
-      }
-    });
+            
+          });   
+          
+          console.log('test');
+          setcompleteUsers(completeUsersArr);
+          console.log(completeUsers)
+        }
+      });
+    }
+    
+    getData();
 
-  }); 
+  }, [posts]); 
+
+  function textChange(value) {
+    setfilteredText( value );
+  }
+
+  let filteredUsers = [];
+
+  completeUsers.forEach( user => {
+    if( user.name.toLowerCase().includes(filteredtext.toLowerCase()) 
+      || user.username.toLowerCase().includes(filteredtext.toLowerCase())
+    ){
+      filteredUsers.push(user);
+    }    
+  });
 
   return (
     <div className="principal-container">
       <h1>Search for Users</h1>
       <div className="users-table">
-        <SearchBar />
+        <SearchBar onTextChange={textChange} filteredtext={filteredtext}/>
         <div className="card-container">
         {
-          completeUsers.map(user => {            
+          filteredUsers.map(user => {            
           return <UserCard key={user.userId} user={user} />
           })
         }
